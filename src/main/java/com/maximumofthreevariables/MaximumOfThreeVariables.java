d2fcda1e284d12c12802779bc6589e905b9c8821
madhuvayya <madhu.vayya@gmail.com>
2020-05-07 09:18:13 +0530

    Added feature to find maximum of three float values

--- src/main/java/com/maximumofthreevariables/MaximumOfThreeVariables.java
+++ src/main/java/com/maximumofthreevariables/MaximumOfThreeVariables.java
@@ -10,4 +10,13 @@ public class MaximumOfThreeVariables {
             max = z;
         return max;
     }
+
+    public Float findMaximumValue(Float x, Float y, Float z) {
+        Float max = x;
+        if(y.compareTo(max)>0)
+            max = y;
+        if(z.compareTo(max)>0)
+            max = z;
+        return max;
+    }
 }
--- src/test/java/com/maximumofthreevariables/MaximumOfThreeVariablesTest.java
+++ src/test/java/com/maximumofthreevariables/MaximumOfThreeVariablesTest.java
@@ -6,16 +6,44 @@ import org.junit.Test;
 public class MaximumOfThreeVariablesTest {
 
     @Test
-    public void givenThreeIntegers_WhenMaximumFound_ShouldReturnMaximumValue() {
+    public void givenThreeIntegers_WhenMaximumFound_ShouldReturnThirdPositionValue() {
         MaximumOfThreeVariables maximumOfThreeVariables = new MaximumOfThreeVariables();
         Integer maximumValue = maximumOfThreeVariables.findMaximumValue(1, 2, 3);
         Assert.assertEquals((Object) 3,maximumValue);
     }
 
     @Test
-    public void givenThreeIntegersDifferentOrder_WhenMaximumFound_ShouldReturnMaximumValue() {
+    public void givenThreeIntegers_WhenMaximumFound_ShouldReturnSecondPositionValue() {
         MaximumOfThreeVariables maximumOfThreeVariables = new MaximumOfThreeVariables();
         Integer maximumValue = maximumOfThreeVariables.findMaximumValue(1, 3, 2);
         Assert.assertEquals((Object) 3,maximumValue);
+    }
+
+    @Test
+    public void givenThreeIntegers_WhenMaximumFound_ShouldReturnFirstPositionValue() {
+        MaximumOfThreeVariables maximumOfThreeVariables = new MaximumOfThreeVariables();
+        Integer maximumValue = maximumOfThreeVariables.findMaximumValue(3, 2, 1);
+        Assert.assertEquals((Object) 3,maximumValue);
+    }
+
+    @Test
+    public void givenThreeFloatValues_WhenMaximumFound_ShouldReturnThirdPositionValue() {
+        MaximumOfThreeVariables maximumOfThreeVariables = new MaximumOfThreeVariables();
+        Float maximumValue = maximumOfThreeVariables.findMaximumValue(1.1f, 2.2f, 3.3f);
+        Assert.assertEquals((Object) 3.3f,maximumValue);
+    }
+
+    @Test
+    public void givenThreeFloatValues_WhenMaximumFound_ShouldReturnSecondPositionValue() {
+        MaximumOfThreeVariables maximumOfThreeVariables = new MaximumOfThreeVariables();
+        Float maximumValue = maximumOfThreeVariables.findMaximumValue(1.1f, 3.3f, 2.2f);
+        Assert.assertEquals((Object) 3.3f,maximumValue);
+    }
+
+    @Test
+    public void givenThreeFloatValues_WhenMaximumFound_ShouldReturnFirstPositionValue() {
+        MaximumOfThreeVariables maximumOfThreeVariables = new MaximumOfThreeVariables();
+        Float maximumValue = maximumOfThreeVariables.findMaximumValue(3.3f, 2.2f, 1.1f);
+        Assert.assertEquals((Object) 3.3f,maximumValue);
     }
 }
